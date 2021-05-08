a89c0a1e158f22c1846b7f4ddba16be857e219b2
tm <tm@tyrellcorp.work>
2021-05-08 15:19:23 +0300

    name protocol constants

--- ha-bt/device/elk_bledom.py
+++ ha-bt/device/elk_bledom.py
@@ -29,16 +29,16 @@ class LedRgb:
 
     def _switch_power(self, state):
         if state == 'OFF':
-            self._send_packet(b'\x04', b'\x00')
+            self._send_packet(Power.COMMAND, Power.OFF)
         else:
-            self._send_packet(b'\x04', b'\x01')
+            self._send_packet(Power.COMMAND, Power.ON)
 
     def _set_light(self, new_state):
         color = new_state.get('color')
         if color is not None:
             self._send_packet(
-                b'\x05',
-                b'\x03',
+                Color.COMMAND,
+                Color.SUB_COMMAND,
                 bytes([color['r']]),
                 bytes([color['g']]),
                 bytes([color['b']]),
@@ -46,7 +46,7 @@ class LedRgb:
 
         brightness = new_state.get('brightness')
         if brightness is not None:
-            self._send_packet(b'\x01', bytes([brightness]))
+            self._send_packet(Brightness.COMMAND, bytes([brightness]))
 
     def _get_state_handle(self):
         self.device = bluepy.btle.Peripheral(self.mac)
@@ -62,3 +62,18 @@ class LedRgb:
     def _send_packet(self, command, sub_command, arg_1=ZERO, arg_2=ZERO, arg_3=ZERO):
         packet = b'\x7e\x00' + command + sub_command + arg_1 + arg_2 + arg_3 + b'\x00\xef'
         self.handle.write(packet)
+
+
+class Power:
+    COMMAND = b'\x04'
+    ON = b'\x01'
+    OFF = b'\x00'
+
+
+class Brightness:
+    COMMAND = b'\x01'
+
+
+class Color:
+    COMMAND = b'\x05'
+    SUB_COMMAND = b'\x03'

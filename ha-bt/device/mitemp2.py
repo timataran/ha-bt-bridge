d38bc8e3c3854216816ff019a117652f25bedfd7
timataran <timataran@gmail.com>
2021-05-23 15:43:01 +0300

    base class for devices

--- ha-bt/device/mitemp2.py
+++ ha-bt/device/mitemp2.py
@@ -1,12 +1,5 @@
-import logging
-
-_LOGGER = logging.getLogger(__name__)
+from device.base import DeviceBase
 
 
-class MiTemp2:
-    def __init__(self, config):
-        self.config = config
-        self.bridge = None
-
-    def connect(self, bridge):
-        self.bridge = bridge
+class MiTemp2(DeviceBase):
+    pass

64d0160f62e54a299e7e84c2184ec9729e9bfa29
timataran <timataran@gmail.com>
2025-07-16 00:30:47 +0300

    support for sensor with other firmware

--- ha-bt/device/mitemp/connection.py
+++ ha-bt/device/mitemp/connection.py
@@ -1,12 +1,44 @@
 from bluepy.btle import Peripheral, DefaultDelegate, BTLEDisconnectError
 from device.error import BTConnectError
 
+_FIRMWARE_VERSION_HANDLE = 0x12
+_V2_DATA_HANDLE = 0x33
+
 
 class Connection:
     def __init__(self, mac):
         self.mac = mac
 
     def read(self):
+        device = None
+
+        try:
+            device = Peripheral(self.mac)
+            version = self._get_device_version(device)
+
+            return self.get_v1_data(device) if version == 1 else self.get_v2_data(device)
+
+        except BTLEDisconnectError as error:
+            raise BTConnectError(f"{error}")
+
+        finally:
+            if device is not None:
+                device.disconnect()
+
+    @staticmethod
+    def _get_device_version(device):
+        hex_value = device.readCharacteristic(_FIRMWARE_VERSION_HANDLE)
+        version = str(hex_value, 'utf-8')
+
+        if version.startswith('1.'):
+            return 1
+        elif version.startswith('2.'):
+            return 2
+        else:
+            raise BTConnectError(f"Unknown device version: {version}")
+
+    @staticmethod
+    def get_v1_data(device):
         received_data = []
 
         class NotificationHandler(DefaultDelegate):
@@ -16,23 +48,16 @@ class Connection:
             def handleNotification(self, _, data):
                 received_data.append(data)
 
-        device = None
+        device.writeCharacteristic(0x0038, b'\x01\x00', True)
+        device.writeCharacteristic(0x0046, b'\xf4\x01\x00', True)
+        device.withDelegate(NotificationHandler())
+        device.waitForNotifications(10)
 
-        try:
-            device = Peripheral(self.mac)
-            device.writeCharacteristic(0x0038, b'\x01\x00', True)
-            device.writeCharacteristic(0x0046, b'\xf4\x01\x00', True)
-            device.withDelegate(NotificationHandler())
-            device.waitForNotifications(10)
+        if len(received_data) > 0:
+            return received_data.pop()
+        else:
+            raise BTConnectError('Failed to retrieve sensor data')
 
-            if len(received_data) > 0:
-                return received_data.pop()
-            else:
-                raise BTConnectError('Failed to retrieve sensor data')
-
-        except BTLEDisconnectError as error:
-            raise BTConnectError(f"{error}")
-
-        finally:
-            if device is not None:
-                device.disconnect()
+    @staticmethod
+    def get_v2_data(device):
+        return device.readCharacteristic(_V2_DATA_HANDLE)
--- ha-bt/test/device/mitemp/test_connection.py
+++ ha-bt/test/device/mitemp/test_connection.py
@@ -4,6 +4,10 @@ from device.mitemp.connection import Connection
 from device.error import BTConnectError
 from bluepy.btle import BTLEDisconnectError
 
+_V1_RESPONSE = '1.0.0_0109'.encode('utf-8')
+_V2_RESPONSE = '2.1.1_0159'.encode('utf-8')
+_V3_RESPONSE = '3.3.3_3333'.encode('utf-8')
+
 
 # noinspection PyBroadException
 @patch('device.mitemp.connection.Peripheral')
@@ -16,9 +20,20 @@ class TestConnection(TestCase):
         except:
             pass
 
-        peripheral_mock.ssert_called_with('mac_value')
+        peripheral_mock.assert_called_with('mac_value')
 
-    def test_throw_custom_error_on_no_data_received(self, _):
+    def test_throw_custom_error_on_unknown_device_version(self, peripheral_mock):
+        peripheral_mock.return_value.readCharacteristic.return_value = _V3_RESPONSE
+        connection = Connection('mac_value')
+
+        with self.assertRaises(BTConnectError) as error_context:
+            connection.read()
+        error = error_context.exception
+
+        self.assertEqual('Unknown device version: 3.3.3_3333', str(error))
+
+    def test_throw_custom_error_on_no_data_received(self, peripheral_mock):
+        peripheral_mock.return_value.readCharacteristic.return_value = _V1_RESPONSE
         connection = Connection('mac_value')
 
         with self.assertRaises(BTConnectError) as error_context:
